1d6f165ff12769e56f5b743177246e4cc4452228
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2020-11-22 13:13:29 -0300

    Upload dos exercícios de update e delete
    Exercício  de Banco de Dados sobre a atualização e exclusão (update e delete) de dados do bdAmbulatorio

--- insert-delete e update BDAmbulatorio.sql
+++ insert-delete e update BDAmbulatorio.sql
@@ -1,7 +1,7 @@
 use bdAmbulatorio
 
 insert into tbpacientes(nome,sexo,idade,doenca_inicial)
-values('Domênica Santos','F','20','Gripe')
+values('DomÃªnica Santos','F','20','Gripe')
 	 ,('Camila da Silva','M','19','Covid-19')
 	 ,('Bruna Ferreira','F','25','Sarampo')
 select * from tbpacientes
@@ -18,13 +18,13 @@ delete from tbdoencas
 where descricao = 'Asma'
 
 insert into tbambulatorios(numero,andar,capacidade)
-values('2000','12º',400)
-	 ,('1500A','8º',300)
-	 ,('550','1º',200)
+values('2000','12Âº',400)
+	 ,('1500A','8Âº',300)
+	 ,('550','1Âº',200)
 select * from tbambulatorios
 
 update tbambulatorios
-set andar = '10º'
+set andar = '10Âº'
 where idambulatorios = 3
 
 insert into tbespecialidades(descricao)
@@ -39,9 +39,9 @@ where idespecialidades = 20
 
 insert into tbmedicos(nome,crm,salario,idade,idambulatorios,idespecialidades)
 values('Dr. Carlos Jobim','52-71557-3',12000,50,1,10)
-	 ,('Dr. André Firmino','50-12345-6',22000.57,63,1,10)
+	 ,('Dr. AndrÃ© Firmino','50-12345-6',22000.57,63,1,10)
 	 ,('Dra. Ana Maria','10-20201-5',40000.61,42,2,10)
-	 ,('Dr. Jão Carlos','22-20192-7',38001,56,3,20)
+	 ,('Dr. JÃ£o Carlos','22-20192-7',38001,56,3,20)
 select * from tbmedicos
 
 update tbmedicos
@@ -53,10 +53,10 @@ set nome = 'Dra. Joana Souza Silva'
 where idmedicos = 203
 
 insert into tbfuncionarios(nome,idade,sexo,salario,cidade,idambulatorios)
-values('Fernanda Fernandes',34,'F',2000,'São Paulo',1)
+values('Fernanda Fernandes',34,'F',2000,'SÃ£o Paulo',1)
 	 ,('Bruna Souza',25,'F',1700,'Guarulho',1)
 	 ,('Jessica Filho',20,'F',3000,'Sorocaba',1)
-	 ,('Leandro Leonardo',27,'M',2500,'São Paulo',3)
+	 ,('Leandro Leonardo',27,'M',2500,'SÃ£o Paulo',3)
 select * from tbfuncionarios
 
 insert into tbconsultas(da,hora,idmedicos,idpacientes,idambulatorios,iddoencas)
